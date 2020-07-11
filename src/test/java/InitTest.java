6916148c510bbd23b54210c0a57f868f948cdb7f
simon <hanzhenfeng@wuxicloud.com>
2020-07-11 21:38:19 +0800

    init class

--- src/test/java/InitTest.java
+++ src/test/java/InitTest.java
@@ -1,6 +1,12 @@
+import org.junit.Test;
+
 /**
  * @author Simon
  * @date 2020/7/11 21:37
  */
 public class InitTest {
+    @Test
+    public void name() {
+        System.out.println("a");
+    }
 }
